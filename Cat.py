5c1673ae63fb8cf308a9ed80ada39ed8801246a4
etomaki <70105692+etomaki@users.noreply.github.com>
2021-01-30 18:33:36 +0300

    Add files via upload

---
+++ Cat.py
@@ -0,0 +1,14 @@
+class Cat:
+    def __init__(self, name, sex, age):
+        self.name = name
+        self.sex = sex
+        self.age = age
+
+    def getName(self):
+        return self.name
+    
+    def getAge(self):
+        return self.age
+
+    def getSex(self):
+        return self.sex
---
+++ main.py
@@ -0,0 +1,7 @@
+from Cat import Cat
+
+cat_1 = Cat('Барон', 'Мальчик', 2)
+cat_2 = Cat('Сэм', 'Мальчик', 2)
+
+# print(cat_2.name)
+# print(cat_2.getName())
